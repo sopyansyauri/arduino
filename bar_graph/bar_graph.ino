224f08d3086975e940a112ffdb9846f05578f791
sopyan <rifai.sopyan04@gmail.com>
2023-01-16 09:40:31 +0700

    Menambahkan Project Arduino

---
+++ bar_graph/bar_graph.ino
@@ -0,0 +1,25 @@
+const int analogPin = A0;
+const int ledCount = 9;
+int ledPins[] = {2,3,4,5,6,7,8,9,10};
+
+void setup() {
+  // put your setup code here, to run once:
+  for (int thisled = 0; thisled < ledCount; thisled++) {
+    pinMode(ledPins[thisled], OUTPUT);
+  }
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int sensorReading = analogRead(analogPin);
+  int ledLevel = map(sensorReading, 0, 1023, 0, ledCount);
+  for (int thisled = 0; thisled < ledCount; thisled++) {
+    if (thisled < ledLevel) {
+      digitalWrite(ledPins[thisled], HIGH);
+    } else {
+    digitalWrite(ledPins[thisled], LOW);
+    }
+  }
+
+}
